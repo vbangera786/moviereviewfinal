3a2a51d4715b21a46060df67705e296b2377cea8
vbangera786 <vbangera786@gmail.com>
2019-11-29 12:47:44 +0530

    Commit the changes

--- src/main/java/com/example/moviereview/controller/ReviewController.java
+++ src/main/java/com/example/moviereview/controller/ReviewController.java
@@ -1,6 +1,7 @@
 package com.example.moviereview.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,4 +11,10 @@ public class ReviewController {
     public String getMessage(){
         return "Hello World";
     }
+    @GetMapping(value = "/message/{name}")
+    public String anotherMessage(@PathVariable("name") String name){
+        return "Hello World Again";
+    }
+
+
 }
